49033feb3951d4d08e093c36137c5f8954df4543
Alvin Laurent Ertanto <alvin.ertanto@gmail.com>
2023-11-07 21:17:22 +0700

    dark mode with localStorage

--- game.js
+++ game.js
@@ -27,3 +27,29 @@ const submitForm = (e) => {
   password === confirm_password ? console.log(true) : console.log(false)
 
 }
+
+// Set dark mode based on localStorage
+const checkbox = document.getElementById('checkbox')
+const mode = localStorage.getItem('mode')
+
+if (mode === 'on') {
+  // Kalo local storage valuenya true (dark mode on)
+  checkbox.checked = true
+  document.body.classList.toggle('dark')
+} else {
+  // Kalau tidak ada key 'mode' di localStorage
+  localStorage.setItem('mode', 'off')
+}
+
+// Perubahan pada checkbox
+checkbox.addEventListener("change", () => {
+  document.body.classList.toggle('dark')
+
+  if (checkbox.checked) {
+    // Dark mode
+    localStorage.setItem('mode', 'on')
+  } else {
+    // Light mode
+    localStorage.setItem('mode', 'off')
+  }
+})
